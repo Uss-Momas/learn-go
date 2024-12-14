c948acb667eac9bb9189961463ce86334a8e6f6f
ussumane <ussumane.momade@uem.ac.mz>
2024-12-14 18:04:34 +0200

    using simple printf

--- 0-hello-world/cmd/print/main.go
+++ 0-hello-world/cmd/print/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("\"Programming is like building a multilingual puzzle")
+	fmt.Print("\"Programming is like building a multilingual puzzle\n")
 }
---
+++ 0-hello-world/cmd/printf/printf.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("with proper grammar, but the outcome is a piece of art,\n")
+}
